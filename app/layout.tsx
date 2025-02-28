8d4ae5245420ef3f61ded1cea7203e85df77545e
mike <mike@test.com>
2025-02-28 15:00:25 +0000

    added Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 // import type { Metadata } from 'next';
 
 // export const metadata: Metadata = {
@@ -12,7 +13,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body>{children}</body>
+            <body>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
